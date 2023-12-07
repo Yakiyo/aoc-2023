3c6c27ef4f710bd51c3954e136cea595c61a0de2
Yakiyo <yakiyo1206@gmail.com>
2023-12-07 09:28:47 +0000

    chore: some trial

--- src/day_1.ts
+++ src/day_1.ts
@@ -1,15 +1,41 @@
 const isNum = (char: string): boolean => char >= '0' && char <= '9';
 
+function calibrate(input: string[]): number {
+    return input.map((s) => {
+		const c = s.split('');
+		return Number(`${c.find(isNum)}${c.findLast(isNum)}`);
+	})
+	.reduce((acc, curr) => acc + curr, 0)
+}
+
 const input = (await Deno.readTextFile('./inputs/1.txt'))
 	.split(
 		'\n',
 	);
 
-const result = input
-	.map((s) => {
-		const c = s.split('');
-		return Number(`${c.find(isNum)}${c.findLast(isNum)}`);
-	})
-	.reduce((acc, curr) => acc + curr, 0);
+const part1 = calibrate(input);
 
-console.log(result);
+console.log('part 1: ', part1);
+
+const reps = new Map(Object.entries({
+	'one': '1',
+	'two': '2',
+	'three': '3',
+	'four': '4',
+	'five': '5',
+	'six': '6',
+	'seven': '7',
+	'eight': '8',
+	'nine': '9,',
+}));
+
+const newInput = input.map(s => {
+    for (const [k, v] of reps) {
+        s = s.replaceAll(k, `${v}${k}`);
+    }
+    return s;
+})
+
+const part2 = calibrate(newInput);
+
+console.log("part 2: ", part2);
