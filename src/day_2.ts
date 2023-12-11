c167680be35a904118958db90a98bde125a62a1c
Yakiyo <yakiyo1206@gmail.com>
2023-12-11 05:29:46 +0000

    feat: day 2 part 1

---
+++ src/day_2.ts
@@ -0,0 +1,82 @@
+import { readInput } from '../util.ts';
+
+type Game = {
+	id: number;
+	records: GameRecord[];
+};
+
+type GameRecord = {
+	blue: number;
+	red: number;
+	green: number;
+};
+
+const MAX_VALS: GameRecord = {
+	blue: 14,
+	green: 13,
+	red: 12,
+};
+
+const input = (await readInput(2)).split('\n');
+
+export function parseGame(str: string): Game {
+	const s = str.split(':');
+	if (s.length !== 2) throw new Error(`Invalid value? s: ${s}`);
+	const game: Game = {
+		id: Number(s[0].split(' ').at(-1)!),
+		records: s[1].split(';').map((x) => x.trim()).map((x) =>
+			parseRecord(x)
+		),
+	};
+	return game;
+}
+
+export function parseRecord(str: string): GameRecord {
+	const vals = str.trim().split(',').map((s) => s.trim()).map((s) =>
+		s.split(' ')
+	);
+	const colors: GameRecord = {
+		red: 0,
+		blue: 0,
+		green: 0,
+	};
+	vals.forEach((val) => {
+		if (val.length !== 2) {
+			throw new Error(`Invalid value? str: ${str}, val: ${val}`);
+		}
+		const v = Number(val[0]);
+		if (isNaN(v)) {
+			throw new Error(
+				`Why NaN? v: ${val[0]} str: ${str}, val: ${val}`,
+			);
+		}
+		const k = val[1] as keyof GameRecord;
+		if (!(k in colors)) {
+			throw new Error(`key: ${k} not valid color`);
+		}
+		colors[k] = v;
+	});
+	return colors;
+}
+
+export function isValidGame(game: Game): boolean {
+	const records = game.records;
+	for (const record of records) {
+		for (const [k, v] of Object.entries(record)) {
+			if (MAX_VALS[k as keyof GameRecord] < v) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+function partOne(input: string[]) {
+	return input
+		.map(parseGame) // map lines to games
+		.filter(isValidGame) // filter valid games
+		.map((i) => i.id) // only take their ids
+		.reduce((acc, curr) => acc + curr, 0); // add em
+}
+
+console.log(`Part 1: ${partOne(input)}`);
--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,61 @@
-import { assertEquals } from 'https://deno.land/std@0.208.0/assert/mod.ts';
+import { assert, assertEquals } from 'https://deno.land/std@0.208.0/assert/mod.ts';
 import { parse } from './day_1.ts';
+import { isValidGame, parseGame, parseRecord } from './day_2.ts';
 
-Deno.test('Parse test', () => {
+Deno.test('Day 1: parse test', () => {
 	assertEquals(parse('vzxf4tqrljgxmthreejcr'), ['4', '3']);
+});
+
+Deno.test('Day 2: parse record test', () => {
+	assertEquals(parseRecord('1 green, 3 red, 6 blue'), {
+		red: 3,
+		green: 1,
+		blue: 6,
+	});
+	assertEquals(parseRecord('3 green, 6 red'), {
+		red: 6,
+		green: 3,
+		blue: 0,
+	});
+	assertEquals(parseRecord('1 red'), {
+		red: 1,
+		green: 0,
+		blue: 0,
+	});
+});
+
+Deno.test('Day 2: parse game test', () => {
+	assertEquals(
+		parseGame(
+			'Game 7: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+		),
+		{
+			id: 7,
+			records: [
+				{ red: 4, blue: 3, green: 0 },
+				{ red: 1, blue: 6, green: 2 },
+				{ red: 0, blue: 0, green: 2 },
+			],
+		},
+	);
+});
+
+Deno.test('Day 2: is valid game test', () => {
+	assert(isValidGame({
+		id: 7,
+		records: [
+			{ red: 4, blue: 3, green: 0 },
+			{ red: 1, blue: 6, green: 2 },
+			{ red: 0, blue: 0, green: 2 },
+		],
+	}));
+
+	assert(!isValidGame({
+		id: 7,
+		records: [
+			{ red: 14, blue: 3, green: 0 },
+			{ red: 1, blue: 6, green: 2 },
+			{ red: 0, blue: 0, green: 2 },
+		],
+	}));
 });
