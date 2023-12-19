c1a94c3c4b46a4bf0336bdd349f2b4574e72026e
Yakiyo <yakiyo1206@gmail.com>
2023-12-19 18:20:19 +0000

    feat: day 6 part 2

--- src/day_6.ts
+++ src/day_6.ts
@@ -2,9 +2,6 @@ import { readInput } from '../util.ts';
 
 const input = await readInput(6);
 
-const _input = `Time:      7  15   30
-Distance:  9  40  200`;
-
 /**
  * Calculate number of ways to do a race and return
  * the number of races that exceed record time
@@ -32,4 +29,14 @@ function partOne(input: string) {
 	return vals.reduce((acc, curr) => acc * curr, 1);
 }
 
+function partTwo(input: string) {
+	const [time, distance] = input
+		.split('\n')
+		.map((x) => x.split(':')[1].trim())
+		.map((x) => x.split(/ +/).reduce((acc, curr) => acc + curr, ''))
+		.map(Number);
+	return calcWins(time, distance);
+}
+
 console.log(`Part 1: ${partOne(input)}`);
+console.log(`Part 1: ${partTwo(input)}`);
