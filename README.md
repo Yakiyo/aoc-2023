f121222987e74e9c74db8bfe699a21588a243edf
Yakiyo <yakiyo1206@gmail.com>
2023-12-19 17:49:59 +0000

    feat: readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# aoc-2023
+
+Solutions to my adventofcode challenges for 2023. Written in
+typescript and using Deno. Not sure wether i'll end up finishing it or
+just give up like last year.
+
+## challenges that i forfeited
+
+- day 5 part 2 - cz it runs out of memory and too lengthy
+
+## code
+
+To run a day's code, just do
+
+```bash
+$ deno task run <day_number>
+```
+
+it should run it.
--- src/day_5.ts
+++ src/day_5.ts
@@ -48,9 +48,9 @@ class Range {
 		this.range = range - 1;
 	}
 
-    public static parse(line: string) {
-        return new Range(line);
-    }
+	public static parse(line: string) {
+		return new Range(line);
+	}
 
 	/**
 	 * If the provided number falls in this range
@@ -59,84 +59,84 @@ class Range {
 		return value >= this.source && value <= this.source + this.range;
 	}
 
-    /**
-     * Converts the source number to the destination number
-     */
-    public map(source: number): number | null {
-        if (!this.matches(source)) return null;
-        return this.destination + (source - this.source);
-    }
-
+	/**
+	 * Converts the source number to the destination number
+	 */
+	public map(source: number): number | null {
+		if (!this.matches(source)) return null;
+		return this.destination + (source - this.source);
+	}
 }
 
 class AlmanacMap {
-    private ranges: Range[];
-    constructor(input: string) {
-        // we ignore the first line since all the maps are provided in order
-        this.ranges = input.split('\n').slice(1).map(Range.parse);
-    }
+	private ranges: Range[];
+	constructor(input: string) {
+		// we ignore the first line since all the maps are provided in order
+		this.ranges = input.split('\n').slice(1).map(Range.parse);
+	}
 
-    public static parse(input: string) {
-        return new AlmanacMap(input);
-    }
+	public static parse(input: string) {
+		return new AlmanacMap(input);
+	}
 
-    public convert(source: number) {
-        let destination: number | null = null;
-        for (const range of this.ranges) {
-            destination = range.map(source);
-            // if destination finally got a value, we stop the loop
-            if (destination !== null) break; 
-        }
+	public convert(source: number) {
+		let destination: number | null = null;
+		for (const range of this.ranges) {
+			destination = range.map(source);
+			// if destination finally got a value, we stop the loop
+			if (destination !== null) break;
+		}
 
-        // just in case destination is still null, 
-        // source and destination will then be the same value
-        if (destination === null) {
-            destination = source;
-        }
-        return destination;
-    }
+		// just in case destination is still null,
+		// source and destination will then be the same value
+		if (destination === null) {
+			destination = source;
+		}
+		return destination;
+	}
 }
 
 function convert(source: number, maps: AlmanacMap[]): number {
-    let n = source;
-    for (const map of maps) {
-        n = map.convert(n);
-    }
-    return n;
+	let n = source;
+	for (const map of maps) {
+		n = map.convert(n);
+	}
+	return n;
 }
 
 function partOne(input: string) {
-    const inputs = input.split('\n\n');
-    const seeds = inputs[0].replace('seeds: ', '').split(/ +/).map(Number);
+	const inputs = input.split('\n\n');
+	const seeds = inputs[0].replace('seeds: ', '').split(/ +/).map(
+		Number,
+	);
 	const maps = inputs.slice(1).map(AlmanacMap.parse);
-    return Math.min(...seeds.map(n => convert(n, maps)));
+	return Math.min(...seeds.map((n) => convert(n, maps)));
 }
 
 /**
  * This SHOULD theoritically work, but the `seeds` array exceeds
- * 1516 elements (much more but i quit at that point), and eventually 
- * Deno throws the error `invalid array length`, so i assume this 
+ * 1516 elements (much more but i quit at that point), and eventually
+ * Deno throws the error `invalid array length`, so i assume this
  * solution is not feasible. I'm fairly certain some sort of smart
  * and funky solution exists that can do it, guess i gotta go youtubing for it.
  * Till then imma leave it incomplete.
  */
 function partTwo(_input: string) {
-    // const inputs = input.split('\n\n');
-    // const seedPairs = inputs[0].replace('seeds: ', '').split(/ +/).map(Number);
-    // const seeds: number[] = [];
-    // for (let i = 0; i <= seedPairs.length; i += 2) {
-    //     const start = seedPairs[i];
-    //     const range = seedPairs[i + 1] - 1;
-    //     for (let j = 0; j <= range; j++) {
-    //         console.log(seeds.length);
-    //         seeds.push(start + j);
-    //     }
-    // }
+	// const inputs = input.split('\n\n');
+	// const seedPairs = inputs[0].replace('seeds: ', '').split(/ +/).map(Number);
+	// const seeds: number[] = [];
+	// for (let i = 0; i <= seedPairs.length; i += 2) {
+	//     const start = seedPairs[i];
+	//     const range = seedPairs[i + 1] - 1;
+	//     for (let j = 0; j <= range; j++) {
+	//         console.log(seeds.length);
+	//         seeds.push(start + j);
+	//     }
+	// }
 	// const maps = inputs.slice(1).map(AlmanacMap.parse);
-    // return Math.min(...seeds.map(n => convert(n, maps)));
-    return 0;
+	// return Math.min(...seeds.map(n => convert(n, maps)));
+	return 0;
 }
-
 
 console.log(`Part 1: ${partOne(input)}`);
 console.log(`Part 2: ${partTwo(input)}`);
