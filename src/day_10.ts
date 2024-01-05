356a1dcee3d0d4b4998409acfd9dc16060c5e443
Yakiyo <yakiyo1206@gmail.com>
2024-01-05 15:19:56 +0000

    feat: day 10 part 2

--- src/day_10.ts
+++ src/day_10.ts
@@ -2,11 +2,37 @@ import { assertEquals } from 'https://deno.land/std@0.208.0/assert/mod.ts';
 
 /*
  * 1: 6897
- * 2: 0
+ * 2: 367
  */
 
 export function partOne(input: string) {
 	const grid = input.split('\n').map((i) => i.split(''));
+	const { steps } = loop(grid);
+	return steps / 2;
+}
+
+/**
+ * This solution uses Pick's theorem and was heavily inspired from tlareg's solution
+ *
+ * https://en.wikipedia.org/wiki/Pick%27s_theorem
+ * https://github.com/tlareg/advent-of-code/blob/master/src/2023/day10/index.ts#L215
+ */
+export function partTwo(input: string) {
+	const grid = input.split('\n').map((i) => i.split(''));
+	const { steps, path } = loop(grid);
+	const area = shoelace(path);
+	return area - steps / 2 + 1;
+}
+
+type Direction = 'up' | 'right' | 'left' | 'down';
+
+/**
+ * The main shared logic between the two challenges
+ *
+ * It calculates the steps in the loop and also tracks all the
+ * tiles it encounters in the loop
+ */
+function loop(grid: string[][]) {
 	const [y, x] = resolveStart(grid);
 	const branches = getBranches(grid, [y, x]);
 	if (branches.length !== 2) {
@@ -14,9 +40,10 @@ export function partOne(input: string) {
 	}
 	const getVal = ([y, x]: [number, number]) => grid.at(y)?.at(x);
 
-	let prev = [y, x];
+	let prev: [number, number] = [y, x];
 	let current = branches[0];
 	let ch = getVal(current);
+	const path: [number, number][] = [prev];
 	// one step is for the starting point, the other is for the starting branch
 	let steps = 2;
 	while (ch !== 'S') {
@@ -32,6 +59,7 @@ export function partOne(input: string) {
 		} else {
 			next = validBranches(current, grid, nexts);
 		}
+		path.push(current);
 
 		prev = current;
 		current = next;
@@ -40,11 +68,12 @@ export function partOne(input: string) {
 	}
 	// we decrement once since the initial `S` gets counted twice
 	steps--;
-	return steps / 2;
+
+	return {
+		steps,
+		path,
+	};
 }
-
-type Direction = 'up' | 'right' | 'left' | 'down';
-
 /**
  * Find the starting point 'S'
  * returns [row, column] or [y, x]
@@ -161,6 +190,24 @@ function validBranches<T extends [number, number]>(
 	return valids.shift()!;
 }
 
+/**
+ * Based on https://en.wikipedia.org/wiki/Shoelace_formula
+ */
+function shoelace(vertices: [number, number][]): number {
+	let area = 0;
+
+	for (let i = 0; i < vertices.length; i++) {
+		const nextIndex = (i + 1) % vertices.length;
+		const [currentY, currentX] = vertices[i];
+		const [nextY, nextX] = vertices[nextIndex];
+		area += currentX * nextY - currentY * nextX;
+	}
+
+	area = Math.abs(area) / 2;
+
+	return area;
+}
+
 const pointEq = <T extends [number, number]>(a: T, b: T) =>
 	a[0] === b[0] && a[1] === b[1];
 
@@ -174,7 +221,7 @@ const move: Record<
 	down: ([y, x]) => [y + 1, x],
 };
 
-Deno.test('Part One test', () => {
+Deno.test('Part one test', () => {
 	const teststr = `..F7.
 .FJ|.
 SJ.L7
@@ -184,4 +231,18 @@ LJ...`;
 		partOne(teststr),
 		8,
 	);
+});
+
+Deno.test('Part two test', () => {
+	const teststr = `FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L`;
+	assertEquals(partTwo(teststr), 10);
 });
