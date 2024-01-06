1fef60948dac23f36f535aee5110356f7bb3c753
Yakiyo <yakiyo1206@gmail.com>
2024-01-06 18:14:50 +0000

    feat: day 12 part 2

--- src/day_12.ts
+++ src/day_12.ts
@@ -1,6 +1,6 @@
 /*
  * 1: 7047
- * 2: 0
+ * 2: 17391848518844
  */
 
 import { assertEquals } from 'https://deno.land/std@0.208.0/assert/mod.ts';
@@ -18,6 +18,25 @@ export function partOne(input: string) {
 		.reduce((acc, curr) => acc + curr, 0);
 }
 
+export function partTwo(input: string) {
+	return input
+		.split('\n')
+		.map((line) => {
+			const [pattern, nums] = line.split(' ');
+			return [
+				Array(5).fill(pattern).join('?'),
+				Array(5).fill(nums).join(',').split(',').map(Number),
+			] as OnsenData;
+		})
+		.map(getArrangements)
+		.reduce((acc, curr) => acc + curr, 0);
+}
+
+// a cache to remember values of certain entries, so that
+// we don't recalculate the value when the entry is encountered
+// more than once. This makes part 2 significantly faster.
+const cache: Map<string, number> = new Map();
+
 function getArrangements([cfg, nums]: OnsenData): number {
 	if (cfg === '') {
 		return nums.length ? 0 : 1;
@@ -25,6 +44,12 @@ function getArrangements([cfg, nums]: OnsenData): number {
 	if (nums.length === 0) {
 		return cfg.includes('#') ? 0 : 1;
 	}
+
+	const key = `${cfg} ${nums.join(',')}`;
+	if (cache.has(key)) {
+		return cache.get(key)!;
+	}
+
 	let result = 0;
 
 	if (['?', '.'].includes(cfg[0])) {
@@ -46,20 +71,30 @@ function getArrangements([cfg, nums]: OnsenData): number {
 			]);
 		}
 	}
-
+	cache.set(key, result);
 	return result;
 }
 
-export function partTwo(_input: string) {
-	return 0;
-}
-
 Deno.test('Part one test', () => {
-	const teststr = `???.### 1,1,3
+	assertEquals(
+		partOne(`???.### 1,1,3
 .??..??...?##. 1,1,3
 ?#?#?#?#?#?#?#? 1,3,1,6
 ????.#...#... 4,1,1
 ????.######..#####. 1,6,5
-?###???????? 3,2,1`;
-	assertEquals(partOne(teststr), 21);
+?###???????? 3,2,1`),
+		21,
+	);
+});
+
+Deno.test('Part two test', () => {
+	assertEquals(
+		partTwo(`???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1`),
+		525152,
+	);
 });
