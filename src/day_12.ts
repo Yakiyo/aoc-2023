a63f778c678c4503825c900c06bd176f5fff5228
Yakiyo <yakiyo1206@gmail.com>
2024-01-06 18:00:37 +0000

    feat: day 12 part 1

--- src/day_12.ts
+++ src/day_12.ts
@@ -19,32 +19,34 @@ export function partOne(input: string) {
 }
 
 function getArrangements([cfg, nums]: OnsenData): number {
-	if (!cfg.length) {
+	if (cfg === '') {
 		return nums.length ? 0 : 1;
 	}
-	if (!nums.length) {
+	if (nums.length === 0) {
 		return cfg.includes('#') ? 0 : 1;
 	}
 	let result = 0;
 
-	if ('.?'.includes(cfg[0])) {
+	if (['?', '.'].includes(cfg[0])) {
 		result += getArrangements([cfg.slice(1), nums]);
 	}
 
-	if (
-		'#?'.includes(cfg[0]) &&
-		nums[0] <= cfg.length &&
-		cfg.slice(0, nums[0]).includes('.') &&
-		(
-			nums[0] === cfg.length ||
-			cfg[nums[0]] !== '#'
-		)
-	) {
-		result += getArrangements([
-			cfg.slice(nums[0] + 1),
-			nums.slice(1),
-		]);
+	if (['?', '#'].includes(cfg[0])) {
+		if (
+			nums[0] <= cfg.length &&
+			!cfg.slice(0, nums[0]).includes('.') &&
+			(
+				nums[0] === cfg.length ||
+				cfg[nums[0]] !== '#'
+			)
+		) {
+			result += getArrangements([
+				cfg.slice(nums[0] + 1, cfg.length),
+				nums.slice(1, nums.length),
+			]);
+		}
 	}
+
 	return result;
 }
 
@@ -53,12 +55,11 @@ export function partTwo(_input: string) {
 }
 
 Deno.test('Part one test', () => {
-    const teststr = 
-`???.### 1,1,3
+	const teststr = `???.### 1,1,3
 .??..??...?##. 1,1,3
 ?#?#?#?#?#?#?#? 1,3,1,6
 ????.#...#... 4,1,1
 ????.######..#####. 1,6,5
-?###???????? 3,2,1`
-    assertEquals(partOne(teststr), 21);
-})
+?###???????? 3,2,1`;
+	assertEquals(partOne(teststr), 21);
+});
